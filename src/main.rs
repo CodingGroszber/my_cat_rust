b7050c6f6d2b3ec3ea7b1d907f1bd432762a0e66
Neukirchner <laszlo.neukirchner@siemens.com>
2025-03-31 23:10:44 +0200

    Initial commit: Simple text viewer (cat) implementation in Rust

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, Read, Write};
+
+macro_rules! debug_println {
+    ($($arg:tt)*) => {
+        #[cfg(debug_assertions)]
+        println!($($arg)*)
+    }
+}
+
+fn main() {
+    // Get command-line arguments
+    let args: Vec<String> = env::args().collect();
+    debug_println!("\narguments: {:?}\n", args);
+
+    // If no files were provided, read from stdin
+    if args.len() < 2 {
+        if let Err(err) = cat_stdin() {
+            eprintln!("Error reading from stdin: {}", err);
+        }
+    } else {
+        // Process each file
+        for filename in &args[1..] {
+            if let Err(err) = cat_file(filename) {
+                eprintln!("Error processing {}: {}", filename, err);
+            }
+        }
+    }
+}
+
+// Function to display contents of a file
+fn cat_file(filename: &str) -> io::Result<()> {
+    // Open the file
+    let mut file = File::open(filename)?;
+
+    // Copy all bytes from the file to stdout
+    io::copy(&mut file, &mut io::stdout())?;
+
+    Ok(())
+}
+
+// Function to display contents from stdin
+fn cat_stdin() -> io::Result<()> {
+    // Copy all bytes from stdin to stdout
+    io::copy(&mut io::stdin(), &mut io::stdout())?;
+
+    Ok(())
+}
